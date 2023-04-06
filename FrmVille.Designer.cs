503091caefcdf025702b091f9c9d622417bd3f28
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2023-04-06 09:08:15 +0200

    addQuitter

--- FrmVille.Designer.cs
+++ FrmVille.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Quitter = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,11 +42,22 @@
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // Quitter
+            // 
+            this.Quitter.Location = new System.Drawing.Point(671, 377);
+            this.Quitter.Name = "Quitter";
+            this.Quitter.Size = new System.Drawing.Size(75, 23);
+            this.Quitter.TabIndex = 1;
+            this.Quitter.Text = "Quitter";
+            this.Quitter.UseVisualStyleBackColor = true;
+            this.Quitter.Click += new System.EventHandler(this.Quitter_Click);
+            // 
             // FrmVille
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Quitter);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FrmVille";
             this.Text = "FrmAfficherVille";
@@ -57,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Quitter;
     }
 }
